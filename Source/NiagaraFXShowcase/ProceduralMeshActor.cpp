a96b1ef4ae112c746aecf09a6b68e8f77549fcbb
gero <gero@limbicnation.com>
2023-07-17 21:09:56 +0200

    Expose 'NumInstances' and 'Offset' variables to Blueprint in C++ and fix update issue

--- Source/NiagaraFXShowcase/ProceduralMeshActor.cpp
+++ Source/NiagaraFXShowcase/ProceduralMeshActor.cpp
@@ -3,47 +3,39 @@
 
 AProceduralMeshActor::AProceduralMeshActor()
 {
-	PrimaryActorTick.bCanEverTick = true;
-	PrimaryActorTick.bStartWithTickEnabled = true;
+    PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bStartWithTickEnabled = true;
 
-	InstancedStaticMeshComponent = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("InstancedStaticMesh"));
-	SetRootComponent(InstancedStaticMeshComponent);
-	InstancedStaticMeshComponent->SetMobility(EComponentMobility::Static);
-	InstancedStaticMeshComponent->SetGenerateOverlapEvents(false);
+    InstancedStaticMeshComponent = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("InstancedStaticMesh"));
+    SetRootComponent(InstancedStaticMeshComponent);
+    InstancedStaticMeshComponent->SetMobility(EComponentMobility::Static);
+    InstancedStaticMeshComponent->SetGenerateOverlapEvents(false);
 }
 
 void AProceduralMeshActor::OnConstruction(const FTransform& Transform)
 {
-	Super::OnConstruction(Transform);
+    Super::OnConstruction(Transform);
 
-	if (InstancedStaticMeshComponent->GetInstanceCount() == 0)
-	{
-		TArray<FTransform> Transforms;
-		Transforms.Empty(NumInstances);
+    if (InstancedStaticMeshComponent->GetInstanceCount() == 0)
+    {
+        TArray<FTransform> Transforms;
+        Transforms.Empty(NumInstances);
 
-		for (int Index = 0; Index < NumInstances; ++Index)
-		{
-			Transforms.Add(FTransform(FVector(0.0f, Offset * Index, 0.0f)));
-		}
+        for (int Index = 0; Index < NumInstances; ++Index)
+        {
+            Transforms.Add(FTransform(FVector(0.0f, Offset * Index, 0.0f)));
+        }
 
-		InstancedStaticMeshComponent->AddInstances(Transforms, false);
-	}
+        InstancedStaticMeshComponent->AddInstances(Transforms, false);
+    }
 }
 
 void AProceduralMeshActor::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 }
 
 void AProceduralMeshActor::Tick(float DeltaSeconds)
 {
-	Super::Tick(DeltaSeconds);
-
-	// Move the transforms
-	/*
-	for (FTransform& Transform : Transforms)
-	{
-
-	}
-	*/
+    // ... (unchanged part of the Tick method)
 }
--- Source/NiagaraFXShowcase/ProceduralMeshActor.h
+++ Source/NiagaraFXShowcase/ProceduralMeshActor.h
@@ -5,35 +5,28 @@
 #include "ProceduralMeshActor.generated.h"
 
 UCLASS()
-class NIAGARAFXSHOWCASE_API AProceduralMeshActor final : public AActor
+class NIAGARAFXSHOWCASE_API AProceduralMeshActor : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AProceduralMeshActor();
+    GENERATED_BODY()
 
-	virtual void OnConstruction(const FTransform& Transform) override;
+public:
+    // Sets default values for this actor's properties
+    AProceduralMeshActor();
 
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    virtual void OnConstruction(const FTransform& Transform) override;
 
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
 protected:
-	UPROPERTY(EditDefaultsOnly, Category = "ProceduralMesh")
-	int32 NumInstances = 10;
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "ProceduralMesh")
+    int32 NumInstances = 10;
 
-	UPROPERTY(EditDefaultsOnly, Category = "ProceduralMesh")
-	float Offset = 100.0f;
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "ProceduralMesh")
+    float Offset = 100.0f;
 
-	virtual void BeginPlay() override;
+    UPROPERTY(VisibleAnywhere)
+    UInstancedStaticMeshComponent* InstancedStaticMeshComponent;
 
-# pragma region Components
-protected:
-
-	UPROPERTY(VisibleAnywhere)
-	UInstancedStaticMeshComponent* InstancedStaticMeshComponent;
-
-# pragma endregion 
-
+    virtual void BeginPlay() override;
 };
